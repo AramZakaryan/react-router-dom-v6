0087fc5cd3da2367f8193ae371c2aadc8e30207c
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-08-21 14:48:31 +0400

    Result of lesson 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
+import {BrowserRouter, NavLink, Outlet, Route, Routes, useParams} from "react-router-dom";
+
+
+const Profile = () => {
+    const params = useParams()
+    return (
+        <div>
+            <span>This is the Profile Page</span> <br/>
+            <span>id is {params.id}</span>
+        </div>
+    )
+}
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-
-                <NavLink to={"/"}>main</NavLink>-
-                <NavLink to={"/profile"}>profile</NavLink>-
-                <NavLink to={"/login"}>login</NavLink>
-
+                <NavLink to={"/"}>main</NavLink>---
+                <NavLink to={"/login"}>login</NavLink>---
+                <NavLink to={"/profile"}>profile</NavLink>
                 <Routes>
                     <Route path={"/"} element={<div>This is the Main Page</div>}/>
-                    <Route path={"/profile"} element={<div>This is the Profile Page</div>}/>
                     <Route path={"/login"} element={<div>This is the Login Page</div>}/>
-                    <Route path={"/*"} element={<div>This is Another Page</div>}/>
+                    <Route path={"/profile"} element={<div> This is the Profile Page</div>}/>
+                    <Route path={"/profile/:id"} element={<Profile/>}/>
                 </Routes>
-
             </div>
         </BrowserRouter>
     );
