321d289d564e829d8618c93da0c399cf0204fabf
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-08-21 15:13:23 +0400

    Result of lesson 03 (useParams)

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import {BrowserRouter, NavLink, Outlet, Route, Routes, useParams} from "react-ro
 
 
 const Profile = () => {
-    const params = useParams()
+    const params = useParams<"id1" | "id2">()
     return (
         <div>
             <span>This is the Profile Page</span> <br/>
-            <span>id is {params.id}</span>
+            <span>id is {params.id1} and {params.id2}</span>
         </div>
     )
 }
@@ -24,7 +24,7 @@ function App() {
                     <Route path={"/"} element={<div>This is the Main Page</div>}/>
                     <Route path={"/login"} element={<div>This is the Login Page</div>}/>
                     <Route path={"/profile"} element={<div> This is the Profile Page</div>}/>
-                    <Route path={"/profile/:id"} element={<Profile/>}/>
+                    <Route path={"/profile/:id1/:id2"} element={<Profile/>}/>
                 </Routes>
             </div>
         </BrowserRouter>
